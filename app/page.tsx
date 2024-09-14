1d3b5474fd5bc8057fd163cab7022ed76d2d3f82
ebaaay <enoch.toriel@gmail.com>
2024-09-14 06:32:04 -0300

    Subir los archivos nuevamente

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import BlackjackChips from '../components/BlackjackChips';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <BlackjackChips />
+    </div>
+  );
+}
+
+
